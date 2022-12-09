a7cd0cfc5e6e0414edca14a0a1fcf47a471feb43
josephredmond5 <josephredmond5@icloud.com>
2022-12-09 15:53:52 +0000

    fizzBuzz challenge

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,28 @@
-const { fizzBuzz } = require("../src");
+const {fizzBuzz} = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  it("returns Fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(3)).toBe('fizz')
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  it("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toBe('buzz')
+  });
+  
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  it("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe('fizzBuzz')
+    expect(fizzBuzz(30)).toBe('fizzBuzz')
+    expect(fizzBuzz(90)).toBe('fizzBuzz')
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  
+  
+  it("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(4)).toBe(4)
+    expect(fizzBuzz(11)).toBe(11)
+  });
+  
 });
+
+
