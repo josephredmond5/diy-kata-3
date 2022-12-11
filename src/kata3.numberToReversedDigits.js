436edb509c7d2dd62c73c6c02ea8f25a5c7ad697
josephredmond5 <josephredmond5@icloud.com>
2022-12-11 15:40:02 +0000

    reversed number arrays

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,10 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (num) => {
+    const numString = num.toString();
+    const reverseDigits = [''];
+    for(let i = numString.length - 1; i >= 0; i--) {
+        reverseDigits.push(numString[i]);
+    }
+    return reverseDigits;
+};
 
 module.exports = numberToReversedDigits;
--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,7 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    expect("returns a reversed array of that numbers digits")
+  });
 });
