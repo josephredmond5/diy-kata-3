28a5b368552540ba2f992079b3126bc574eee09d
josephredmond5 <josephredmond5@icloud.com>
2022-12-12 12:51:22 +0000

    humanCatDogYears finished

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,22 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (humanYears) => {
+    let catYears = 56;
+    let dogYears = 64;
+
+    for(let i = 1; i < humanYears.length; i++){
+        if (humanYears === 1) {
+          catYears = 15;
+          dogYears = 15;
+        } else if (humanYears === 2) {
+          catYears = 24;
+          dogYears = 24;
+        } else if (humanYears >= 3) {
+          catYears += 4;
+          dogYears += 5;
+        }
+      }
+      return [humanYears, catYears, dogYears];
+    };
+
+
 
 module.exports = humanCatDogYears;
